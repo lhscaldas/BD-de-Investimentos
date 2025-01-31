edfedcdb086e4e0d7a5e9dc843bac2df9cb35adc
lhscaldas <lhscaldas@gmail.com>
2025-01-31 16:09:30 -0300

    melhoria no filtro de ativos

--- desenvolvimento/investimentos/views_ativos.py
+++ desenvolvimento/investimentos/views_ativos.py
@@ -20,6 +20,18 @@ class AtivoListView(LoginRequiredMixin, ListView):
         filtros = {f"{k}__icontains": v for k, v in self.request.GET.items() if v and k in filtros_validos}
         
         return queryset.filter(**filtros)
+    
+    def get_context_data(self, **kwargs):
+        """ Adiciona as opções de filtro ao contexto """
+        context = super().get_context_data(**kwargs)
+        usuario_ativos = Ativo.objects.filter(usuario=self.request.user)
+
+        # Obtém valores únicos de Classe, Subclasse e Banco
+        context['classes_disponiveis'] = usuario_ativos.values_list('classe', flat=True).distinct()
+        context['subclasses_disponiveis'] = usuario_ativos.values_list('subclasse', flat=True).distinct()
+        context['bancos_disponiveis'] = usuario_ativos.values_list('banco', flat=True).distinct()
+
+        return context
 
 class AtivoCreateView(LoginRequiredMixin, CreateView):
     model = Ativo
